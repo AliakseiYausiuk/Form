09792106d8729dd323a10db12a79533cb106c0e4
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-11-30 20:05:38 +0300

    feat: add Routes pages

---
+++ src/component/Pages/Pages.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import {GetStart} from '../GetStart/GetStart';
+import {Login} from '../Login/Login';
+import {Signup} from '../Signup/Signup';
+import {Error404} from '../Error404/Error';
+
+export const Pages = () => {
+    return (
+        <>
+            <Routes>
+                <Route path={'/'} element={<GetStart title={'Get start'}/>}/>
+                <Route path={'/login'} element={<Login title={'Log In'}/>}/>
+                <Route path={'/singup'} element={<Signup title={'Sign Up'}/>}/>
+
+                <Route path={'*'} element={<Error404/>}/>
+            </Routes>
+        </>
+    )
+}
