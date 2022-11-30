0b7c2fc08867a8907bd92a9e942531fb65fd120a
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-11-30 20:02:46 +0300

    feat: add navLink

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import {Button, ButtonGroup, Grid} from '@material-ui/core';
 import styles from './header.module.css'
+import {NavLink} from 'react-router-dom';
 
 
 export const Header = () => {
     return (
         <Grid className={styles.header} container justifyContent={'flex-end'}>
             <ButtonGroup color={'secondary'}>
-                <Button variant={'contained'}>Get Start</Button>
-                <Button variant={'contained'}>Log In</Button>
-                <Button variant={'contained'}>Log Up</Button>
+                <Button variant={'contained'}><NavLink to={'/'} className={styles.link}>Get Start</NavLink></Button>
+                <Button variant={'contained'}><NavLink to={'/login'} className={styles.link}>Log In</NavLink></Button>
+                <Button variant={'contained'}><NavLink to={'/singup'} className={styles.link}>Sign Up</NavLink></Button>
             </ButtonGroup>
         </Grid>
     )
