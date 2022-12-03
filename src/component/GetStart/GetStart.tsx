cd29ff07bfcbda9f506947060ec658329ed8db32
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-12-03 23:32:54 +0300

    feat: add base layout

--- src/component/GetStart/GetStart.tsx
+++ src/component/GetStart/GetStart.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import {Grid} from '@material-ui/core';
+import {Button, Grid} from '@material-ui/core';
 import {Title} from '../Title/Title';
+import {NavLink} from 'react-router-dom';
+import style from './getStart.module.css'
 
 export type PropsTypeGetStart = {
     title: string
@@ -8,8 +10,12 @@ export type PropsTypeGetStart = {
 
 export const GetStart = ({title}: PropsTypeGetStart) => {
     return (
-        <Grid>
+        <Grid container justifyContent={'center'} direction={'column'} alignItems={'center'}>
             <Title title={title}/>
+            <Grid className={style.wrapper} container justifyContent={'center'} direction={'column'} alignItems={'center'}>
+                <Button variant={'contained'} color={'secondary'}><NavLink className={style.link} to={'/login'}>Get
+                    started</NavLink></Button>
+            </Grid>
         </Grid>
     )
 }
