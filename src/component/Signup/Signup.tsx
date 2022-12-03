2e56ed2a91e86e8db75be79479ea840367c1f6f0
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2022-12-03 23:34:44 +0300

    feat: add type and fun save localStore

--- src/component/Signup/Signup.tsx
+++ src/component/Signup/Signup.tsx
@@ -1,33 +1,49 @@
 import React from 'react';
 import {Button, Grid, TextField} from '@material-ui/core';
 import styles from '../Login/login.module.css'
-import {useForm} from 'react-hook-form';
+import {useForm, SubmitHandler} from 'react-hook-form';
 import {Title} from '../Title/Title';
 
 export type PropsTypeSinup = {
     title: string
 }
+export type FormValues = {
+    email: string
+    password: number
+}
 
 
 export const Signup = ({title}: PropsTypeSinup) => {
-    const {register, handleSubmit, formState: {errors, isValid}, reset} = useForm({mode: 'onBlur'});
-    const onSubmit = (data: any) => {
+    console.log(localStorage.getItem('user'))
+
+    const setLocalStorage = (email: string, password: number) => {
+
+        const user = {
+            email,
+            password
+        }
+        localStorage.setItem('user', JSON.stringify(user));
+    }
+
+    const {register, handleSubmit, formState: {errors, isValid}, reset} = useForm<FormValues>({mode: 'onBlur'});
+    const onSubmit: SubmitHandler<FormValues> = (data: any) => {
+        setLocalStorage(data.email, data.password);
         reset();
     }
 
     return (
         <Grid className={styles.wrapper} container direction={'column'}>
             <Title title={title}/>
-            <TextField className={styles.email} label="Email" variant="standard" margin="normal" type={'text'}
-                       {...register('Email', {
+            <TextField className={styles.email} label="Email" variant="standard" margin="normal" type={'email'}
+                       {...register('email', {
                            required: 'Поле обязательно к заполнению',
                            minLength: {
                                value: 2,
                                message: 'Минимум 2 символа'
                            },
                            pattern: /([A-z0-9_.-]{1,})@([A-z0-9_.-]{1,}).([A-z]{2,8})/
-                       })} error={!!errors?.Email}
-                       helperText={errors?.Email?.message ? `${errors.Email.message}` : ''}/>
+                       })} error={!!errors?.email}
+                       helperText={errors?.email?.message ? `${errors.email.message}` : ''}/>
             <TextField className={styles.password} label="Password" variant="standard" type={'password'} margin="normal"
                        {...register('password', {
                            pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[ -/:-@\[-`{-~]).{6,64}$/,
@@ -39,7 +55,7 @@ export const Signup = ({title}: PropsTypeSinup) => {
                        })} error={!!errors?.password}
                        helperText={errors?.password?.message ? `${errors.password.message}` : ''}/>
             <Button className={styles.btn} variant="contained" color={'secondary'}
-                    onClick={handleSubmit(onSubmit)} disabled={!isValid}>Outlined</Button>
+                    onClick={handleSubmit(onSubmit)} disabled={!isValid}>Save</Button>
         </Grid>
     )
 }
